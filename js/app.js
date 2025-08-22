139ca3f88e977d1c2293ca3cc78b27cc7497f541
helenpastras <helenapastras@hotmail.com>
2025-08-22 01:07:57 -0400

    pseudocode edits and log cleanup

--- js/app.js
+++ js/app.js
@@ -1,22 +1,29 @@
 /*-------------- Pseudocode -------------*/
 // Define the word list:.
-// - `harryPotterSpells`: Array of strings with HP spells
+// - `harryPotterSpells`: Array of strings with Harry Potter spells
+// - `lotrCharacters` : Array of strings with Lord of the Rings characters
+// - Select the theme: Harry Potter spells or Lord of the Rings characters
+// - Open selected game screen
 // - Select the word
-//     - Select a **random** word from `harryPotterSpells`.
-//     - Init game variables
-//         - `secretWord`: Stores the randomly selected word from the chosen list.
+//     - Select a **random** word from `harryPotterSpells` or `lotrCharacters`.
+//     - Declare game variables
+//         - `currentTheme` : A variable storing the theme selected by the player in the homepage 
+//         - `secretWord`: A variable storing the randomly selected word from the chosen list.
 //         - `guessedLetters`: An empty array to store the letters guessed.
 //         - `wrongGuesses`: A counter, initialized to `0`.
 //         - `maxWrongGuesses`: A constant, set to `10`.
 //         - `displayWord`: An array of underscores, one for each letter of the `secretWord`.
 // - Game loop/logic
-//     - Display the game status.
+//     - Start the game with the selected theme
 //         - Show the `displayWord`.
+//     - Display the game status.
 //         - Show the number of `wrongGuesses` remaining.
 //         - Show the letters that have already been guessed.
 //     - Get player input
 //         - Prompt user to enter a letter
-//         - Validate input against previously entered values
+//         - Validate `guessInput` for a-z entries only
+//         - Validate `guessInput` against previously entered values (`guessedLetters`).
+//         - Capture user `guessInput`
 //     - Process the guessed letter
 //         - Add new letter to the `guessedLetters` array
 //         - Check is `secrectWord` contains input letter
@@ -25,13 +32,18 @@
 // - End of game
 //     - After each correct guess, check if the `secrectWord` & `displayWord` are the same.
 //         - If yes, the player won.
+//         - Display theme respective Win message
 //             - else if, keep going.
 //         - If the `wrongGuesses` reaches the `maxWrongGuesses` , the player loses. GAME OVER
+//         - Display theme respective Lose message
+// - Restart game 
+//     - Display Play Again button 
+//     - Define restart to clear user input, guessed letters and wrong guesses number, hide themed backgrounds, and return to the theme selection screen.
 // ______________________________________________________________________________________________________
 
 /*-------------- Constants -------------*/
 const harryPotterSpells = ["accio", "alohomora", "lumos", "expecto patronum", "crucio", "wingardium leviosa", "reparo", "obliviate", "expelliarmus", "nox", "petrificus totalus", "protego", "riddikulus"];
-const lotrCharacters = ["frodo", "gandalf", "aragorn", "legolas", "gollum", "sam", "pippin", "mery", "saruman", "eowyn", "gimli", "sauron"];
+const lotrCharacters = ["frodo", "gandalf", "aragorn", "legolas", "gollum", "sam", "pippin", "merry", "saruman", "eowyn", "gimli", "sauron", "boromir"];
 const maxWrongGuesses = 10;
 
 /*---------- Variables (state) ---------*/
@@ -93,12 +105,9 @@ const initGame = (theme) =>{
 const updateDisplay = () =>{
     wordDisplay.textContent = displayWord.join(' ');
     wrongGuessesText.textContent = `Incorrect guesses left: ${maxWrongGuesses - wrongGuesses}`;
-    console.log(wrongGuessesText)
     guessedLettersDisplay.textContent = [...guessedLetters].join(", ");
     hpButton.classList.add("hidden");
     lotrButton.classList.add("hidden");
-
-    console.log(guessedLetters)
 }
 
 const showMessage = (text) => {
@@ -110,7 +119,6 @@ const showMessage = (text) => {
 const validateGuess = () => {
     let guess = guessInput.value.toLowerCase().trim();
     guessInput.value = "";
-    console.log(guess) //remove after validations
 
     if (guess.length !== 1 || !/[a-z]/.test(guess)){
         showMessage("Please enter only a letter.")
@@ -141,22 +149,20 @@ const validateGuess = () => {
 const checkGame = () => {
      if (displayWord.join('')=== secretWord && currentTheme === "harry-potter") {
         // player wins
-        showMessage(`🌟 Well done 🌟! The spell was " ${secretWord} ". Go to Dumbledoor for house points!`)
-        console.log("You won!")
+        showMessage(`🌟 Well done 🌟! The spell was "${secretWord}". Go to Dumbledoor for house points!`)
         restartButton.classList.remove("hidden");
      } else if (displayWord.join('')=== secretWord && currentTheme === "lotr") {
         // player wins
-        showMessage(`🌟 Well done 🌟! The character was " ${secretWord} ". Gandalf would be proud!`)
-        console.log("You won!")
+        showMessage(`🌟 Well done 🌟! The character was "${secretWord}". Gandalf would be proud!`)
         restartButton.classList.remove("hidden");
      } else if (wrongGuesses >= maxWrongGuesses && currentTheme === "harry-potter") {
         // player loses
-        showMessage(`Your magic failed 👎 ! Eat slugs! The spell was " ${secretWord} ".`);
+        showMessage(`Your magic failed 👎 ! Eat slugs! The spell was "${secretWord}".`);
         gameScreen.classList.add("hidden");
         restartButton.classList.remove("hidden");
      } else if (wrongGuesses >= maxWrongGuesses && currentTheme === "lotr") {
         // player loses
-        showMessage(`You failed 👎 ! The orks are coming for you! The spell was " ${secretWord} ".`);
+        showMessage(`You failed 👎 ! The orks are coming for you! The spell was "${secretWord}".`);
         gameScreen.classList.add("hidden");
         restartButton.classList.remove("hidden");
      }
